9b3e3271905c00249d44ff2aad784164101b7439
ãhwijae33 <gnltps18@naver.com>
2025-01-06 14:55:02 +0900

    feat: pin-up 초기세팅

---
+++ src/main/java/kr/co/pinup/PinupApplication.java
@@ -0,0 +1,13 @@
+package kr.co.pinup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/pinup/PinupApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.pinup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
