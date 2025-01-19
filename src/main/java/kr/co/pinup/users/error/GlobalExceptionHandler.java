04d597db3caad92e49b43f3650e1dbefcdad5f65
kimdohee58 <kimdohee58@gmail.com>
2025-01-20 01:27:34 +0900

    delete : yml

--- src/main/java/kr/co/pinup/users/error/GlobalExceptionHandler.java
+++ src/main/java/kr/co/pinup/users/error/GlobalExceptionHandler.java
@@ -26,6 +26,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
+//    @ExceptionHandler(Exception.class)
+//    public ModelAndView handleGenericException(Exception e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("error");
+//        modelAndView.addObject("message", "서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.");
+//        return modelAndView;
+//    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception e) {
         log.error("서버에서 예외가 발생했습니다: ", e.getMessage());
