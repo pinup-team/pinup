dfeea4fabaa8594f7bf6c641bcc0cbd52f732aa3
whoamixzerone <whoamixzerone@gmail.com>
2025-01-31 20:45:12 +0900

    feat: 전체 조회 작성시간, id desc & test 수정

--- src/main/java/kr/co/pinup/notices/repository/NoticeRepository.java
+++ src/main/java/kr/co/pinup/notices/repository/NoticeRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
-    List<Notice> findAllByOrderByCreatedAtDesc();
+    List<Notice> findAllByOrderByCreatedAtDescIdDesc();
 }
