d0df6da46e62bc2a8ce0432f270055875c40c209
kimdohee58 <kimdohee58@gmail.com>
2025-02-03 19:43:33 +0900

    fix : common Exception extends 수정

--- src/main/java/kr/co/pinup/exception/common/UnauthorizedException.java
+++ src/main/java/kr/co/pinup/exception/common/UnauthorizedException.java
@@ -1,10 +1,18 @@
 package kr.co.pinup.exception.common;
 
+import kr.co.pinup.exception.GlobalCustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends RuntimeException {
+public class UnauthorizedException extends GlobalCustomException {
+
+    private static final String DEFAULT_MESSAGE = "인증 정보가 없습니다.";
+
+    public UnauthorizedException() {
+        super(DEFAULT_MESSAGE);
+    }
+
     public UnauthorizedException(String message) {
         super(message);
     }
