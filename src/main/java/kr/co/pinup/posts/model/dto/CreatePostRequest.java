f2f2738ca30d37a5a18f528cd95259e20b783783
ãhwijae33 <gnltps18@naver.com>
2025-02-11 18:20:13 +0900

    Fix: 제목 및 내용 필드 유효성 검증 강화
    - 제목과 내용 필드에 대해 @NotEmpty에서 @NotBlank로 변경하여 공백 문자열도 검증하도록 수정
    - 공백 문자열을 허용하지 않도록 유효성 검사 강화

--- src/main/java/kr/co/pinup/posts/model/dto/CreatePostRequest.java
+++ src/main/java/kr/co/pinup/posts/model/dto/CreatePostRequest.java
@@ -1,8 +1,7 @@
 package kr.co.pinup.posts.model.dto;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import kr.co.pinup.postImages.model.dto.PostImageRequest;
 import lombok.*;
 
 @Builder
@@ -11,11 +10,11 @@ public record CreatePostRequest(
         Long storeId,
         Long userId,
 
-        @NotEmpty(message = "제목을 입력해주세요.")
+        @NotBlank(message = "제목을 입력해주세요.")
         @Size(min = 1, max = 100, message = "제목은 1자 이상, 100자 이하로 입력해주세요.")
         String title,
 
-        @NotEmpty(message = "내용을 입력해주세요.")
+        @NotBlank(message = "내용을 입력해주세요.")
         @Size(min = 1, max = 2000, message = "내용은 1자 이상, 2000자 이하로 입력해주세요.")
         String content
 ) {
