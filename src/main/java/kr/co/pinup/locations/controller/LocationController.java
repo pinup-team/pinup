ca2da27b375f541e1229b61500dbf0229de1afdc
포키통 <yujusul@gmail.com>
2025-02-22 15:24:12 +0900

    chore: 현재 로직이 존재하지 않는 클래스 파일 삭제

--- src/main/java/kr/co/pinup/locations/controller/LocationController.java
+++
@@ -1,4 +0,0 @@
-package kr.co.pinup.locations.controller;
-
-public class LocationController {
-}
--- src/test/java/kr/co/pinup/stores/controller/StoresApiControllerTest.java
+++
@@ -1,4 +0,0 @@
-package kr.co.pinup.stores.controller;
-
-public class StoresApiControllerTest {
-}
