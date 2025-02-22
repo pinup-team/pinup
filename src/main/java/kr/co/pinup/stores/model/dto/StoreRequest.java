4e7b5eca3ffe24e753ba660aeb970e861005752a
포키통 <yujusul@gmail.com>
2025-02-22 15:25:38 +0900

    chore: StoreRequest categoryId, locationId 에 @NotNull 추가

--- src/main/java/kr/co/pinup/stores/model/dto/StoreRequest.java
+++ src/main/java/kr/co/pinup/stores/model/dto/StoreRequest.java
@@ -8,8 +8,8 @@ import java.time.LocalDate;
 public record StoreRequest(
         @NotBlank String name,
         @NotBlank String description,
-        Long categoryId,
-        Long locationId,
+        @NotNull Long categoryId,
+        @NotNull Long locationId,
         @NotNull LocalDate startDate,
         @NotNull LocalDate endDate,
         @NotBlank String imageUrl
