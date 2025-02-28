95fd1e73e5242d5d13aa7fcc21f8415a2fb650c8
ãhwijae33 <gnltps18@naver.com>
2025-02-28 13:39:36 +0900

    feat: 인증 방식 변경
    - CommentApiController에서 @LoginMember를 @AuthenticationPrincipal로 변경

--- src/main/java/kr/co/pinup/comments/controller/CommentApiController.java
+++ src/main/java/kr/co/pinup/comments/controller/CommentApiController.java
@@ -5,12 +5,12 @@ import jakarta.validation.Valid;
 import kr.co.pinup.comments.model.dto.CommentResponse;
 import kr.co.pinup.comments.model.dto.CreateCommentRequest;
 import kr.co.pinup.comments.service.CommentService;
-import kr.co.pinup.custom.loginMember.LoginMember;
 import kr.co.pinup.members.model.dto.MemberInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,7 +28,7 @@ public class CommentApiController {
     }
 
     @PostMapping("/{postId}")
-    public ResponseEntity<CommentResponse> createComment(@LoginMember MemberInfo memberInfo, @PathVariable Long postId, @Valid @RequestBody CreateCommentRequest createCommentRequest) {
+    public ResponseEntity<CommentResponse> createComment(@AuthenticationPrincipal MemberInfo memberInfo, @PathVariable Long postId, @Valid @RequestBody CreateCommentRequest createCommentRequest) {
         return new ResponseEntity<>(commentService.createComment(memberInfo, postId, createCommentRequest), HttpStatus.CREATED);
     }
 
