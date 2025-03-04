059d93ad7fc24b7b2333406b5bec081479a54d9e
ãhwijae33 <gnltps18@naver.com>
2025-03-04 19:03:08 +0900

    feat : 불필요한 로직 제거 및 게시글/댓글 수정, 삭제 버튼 본인 확인 방식 변경
    - PostController에서 member 객체 조회 후 처리하는 불필요한 로직 제거
    - #authentication.principal.username을 사용하여 로그인한 사용자와 게시글/댓글의 본인 여부를 비교하도록 수정
    - 수정된 방식에 맞춰 게시글 및 댓글의 수정/삭제 버튼을 본인만 표시하도록 변경

--- src/main/java/kr/co/pinup/posts/controller/PostController.java
+++ src/main/java/kr/co/pinup/posts/controller/PostController.java
@@ -1,16 +1,11 @@
 package kr.co.pinup.posts.controller;
 
 import kr.co.pinup.comments.service.CommentService;
-import kr.co.pinup.members.Member;
-import kr.co.pinup.members.exception.MemberNotFoundException;
-import kr.co.pinup.members.model.dto.MemberInfo;
-import kr.co.pinup.members.repository.MemberRepository;
 import kr.co.pinup.postImages.service.PostImageService;
 import kr.co.pinup.posts.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,31 +24,19 @@ public class PostController {
     private final PostService postService;
     private final CommentService commentService;
     private final PostImageService postImageService;
-    private final MemberRepository memberRepository;
 
     @GetMapping("/list/{storeId}")
-    public String getAllPosts(@AuthenticationPrincipal MemberInfo memberInfo, @PathVariable Long storeId, Model model) {
-        if (memberInfo != null) {
-            Member member = memberRepository.findByNickname(memberInfo.nickname())
-                    .orElseThrow(() -> new MemberNotFoundException(memberInfo.nickname() + "님을 찾을 수 없습니다."));
-            model.addAttribute("member", member);
-        }
+    public String getAllPosts(@PathVariable Long storeId, Model model) {
         model.addAttribute("posts", postService.findByStoreId(storeId));
         model.addAttribute("storeId", storeId);
         return VIEW_PATH + "/list";
     }
 
     @GetMapping("/{postId}")
-    public String getPostById(@AuthenticationPrincipal MemberInfo memberInfo,@PathVariable Long postId, Model model) {
-        if (memberInfo != null) {
-            Member member = memberRepository.findByNickname(memberInfo.nickname())
-                    .orElseThrow(() -> new MemberNotFoundException(memberInfo.nickname() + "님을 찾을 수 없습니다."));
-            model.addAttribute("member", member);
-        }
+    public String getPostById(@PathVariable Long postId, Model model) {
         model.addAttribute("post", postService.getPostById(postId));
         model.addAttribute("comments", commentService.findByPostId(postId));
         model.addAttribute("images", postImageService.findImagesByPostId(postId));
-
         return VIEW_PATH + "/detail";
     }
 
