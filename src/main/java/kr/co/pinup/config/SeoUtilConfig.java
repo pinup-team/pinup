4bf90f42fd59a47b0eb6ff0e8bd0db555e44ce04
ãhwijae33 <gnltps18@naver.com>
2025-03-31 03:03:39 +0900

    refactor: SeoUtil → SeoUtilConfig로 클래스명 변경 및 레이아웃 템플릿 수정
    - SeoUtil.java 파일명을 SeoUtilConfig.java로 변경하여 역할 명확화
    - layout.html 내 @seoUtil 사용 부분 반영을 위해 템플릿 코드 수정

--- src/main/java/kr/co/pinup/config/SeoUtilConfig.java
+++ src/main/java/kr/co/pinup/config/SeoUtilConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SeoUtil {
+public class SeoUtilConfig {
 
     @Value("${app.domain:}")
     private String baseDomain;
