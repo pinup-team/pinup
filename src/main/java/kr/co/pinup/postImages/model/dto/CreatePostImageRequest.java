fef7a36990cb33ba76431c60b11cb79d10109168
ãhwijae33 <gnltps18@naver.com>
2025-04-05 14:10:43 +0900

    refactor: PostImageRequest 제거 및 생성/수정 DTO 분리
    - PostImageRequest 삭제
    - CreatePostImageRequest, UpdatePostImageRequest 추가
    - 이미지 요청 목적(생성 vs 수정) 명확하게 분리
    - 향후 유효성 검증 및 서비스 로직 분리 용이

--- src/main/java/kr/co/pinup/postImages/model/dto/CreatePostImageRequest.java
+++ src/main/java/kr/co/pinup/postImages/model/dto/CreatePostImageRequest.java
@@ -1,5 +1,7 @@
 package kr.co.pinup.postImages.model.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,12 +10,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostImageRequest {
+public class CreatePostImageRequest {
+    @NotEmpty(message = "이미지는 필수입니다.")
+    @Size(min = 2, max = 5, message = "이미지는 최소 2장 이상, 최대 5장까지 등록 가능합니다.")
     private List<MultipartFile> images;
-    private List<String> imagesToDelete;
 }
