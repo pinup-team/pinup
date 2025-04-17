52aafebe8c65becb8fc1eb85e4e12116c11c36c4
whoamixzerone <whoamixzerone@gmail.com>
2025-04-17 15:12:04 +0900

    test: unit & integration & e2e 테스트 새로 추가 작성

--- src/test/java/kr/co/pinup/notices/controller/NoticeControllerTest.java
+++ src/test/java/kr/co/pinup/notices/controller/NoticeControllerTest.java
@@ -1,122 +1,169 @@
 package kr.co.pinup.notices.controller;
 
-import kr.co.pinup.config.SecurityConfigTest;
-import kr.co.pinup.config.SeoUtilConfigTest;
-import kr.co.pinup.members.custom.WithMockMember;
-import kr.co.pinup.members.model.enums.MemberRole;
-import kr.co.pinup.members.service.MemberService;
+import kr.co.pinup.members.model.dto.MemberResponse;
+import kr.co.pinup.notices.exception.NoticeNotFound;
 import kr.co.pinup.notices.model.dto.NoticeResponse;
 import kr.co.pinup.notices.service.NoticeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.IntStream;
 
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Import({SecurityConfigTest.class, SeoUtilConfigTest.class})
-@WebMvcTest(NoticeController.class)
+@WebMvcTest(controllers = NoticeController.class,
+        excludeAutoConfiguration = {
+                ThymeleafAutoConfiguration.class,
+                SecurityAutoConfiguration.class,
+                OAuth2ClientAutoConfiguration.class
+        })
 class NoticeControllerTest {
 
-    static final String VIEW_PATH = "views/notices";
+    private static final String VIEW_PATH = "views/notices";
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @MockitoBean
-    NoticeService noticeService;
+    private NoticeService noticeService;
 
-    @MockitoBean
-    MemberService memberService;
-
+    @DisplayName("공지사항 list 페이지 뷰를 반환한다.")
     @Test
-    @DisplayName("공지사항 리스트 페이지 이동")
-    void listPage() throws Exception {
-        // given
-        List<NoticeResponse> mockNotices = IntStream.range(0, 10)
-                .mapToObj(i -> NoticeResponse.builder()
-                        .title("공지사항 제목 " + (10 - i))
-                        .content("공지사항 내용 " + (10 - i))
-                        .build())
-                .toList();
+    void returnListView() throws Exception {
+        // Arrange
+        NoticeResponse response1 = createNoticeResponse("title 1", "content 1",
+                LocalDateTime.of(2025, 1, 1, 0, 0, 0));
+        NoticeResponse response2 = createNoticeResponse("title 2", "content 2",
+                LocalDateTime.of(2025, 1, 1, 1, 0, 0));
+        List<NoticeResponse> responses = List.of(response2, response1);
 
-        // when
-        when(noticeService.findAll()).thenReturn(mockNotices);
+        given(noticeService.findAll()).willReturn(responses);
 
-        // expected
+        // Act & Assert
         mockMvc.perform(get("/notices"))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name(VIEW_PATH + "/list"))
                 .andExpect(model().attributeExists("notices"))
-                .andExpect(model().attribute("notices", is(mockNotices)))
-                .andDo(print());
+                .andExpect(model().attribute("notices", hasSize(2)));
+
+        then(noticeService).should(times(1))
+                .findAll();
     }
 
+    @DisplayName("공지사항 create 페이지 뷰를 반환한다.")
     @Test
-    @WithMockMember(role = MemberRole.ROLE_ADMIN)
-    @DisplayName("공지사항 생성 페이지 이동")
-    void newPage() throws Exception {
-        // given
+    void returnCreateView() throws Exception {
+        // Arrange
 
-        // expected
+        // Act & Assert
         mockMvc.perform(get("/notices/new"))
+                .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(view().name(VIEW_PATH + "/create"))
-                .andDo(print());
+                .andExpect(view().name(VIEW_PATH + "/create"));
     }
 
+    @DisplayName("공지사항 detail 페이지 뷰를 반환한다.")
     @Test
-    @DisplayName("공지사항 상세 페이지 이동")
-    void detailPage() throws Exception {
-        // given
+    void returnDetailView() throws Exception {
+        // Arrange
         long noticeId = 1L;
-        NoticeResponse mockNotice = NoticeResponse.builder()
-                .title("공지사항 제목")
-                .content("공지사항 내용")
-                .build();
+        NoticeResponse response = createNoticeResponse("title 1", "content 1",
+                LocalDateTime.of(2025, 1, 1, 0, 0, 0));
 
-        // when
-        when(noticeService.find(noticeId)).thenReturn(mockNotice);
+        given(noticeService.find(noticeId)).willReturn(response);
 
-        // expected
+        // Act & Assert
         mockMvc.perform(get("/notices/{noticeId}", noticeId))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name(VIEW_PATH + "/detail"))
-                .andExpect(model().attributeExists("notice"))
-                .andExpect(model().attribute("notice", is(mockNotice)))
-                .andDo(print());
+                .andExpect(model().attributeExists("notice"));
+
+        then(noticeService).should(times(1))
+                .find(noticeId);
     }
 
+    @DisplayName("존재하지 않는 ID로 detail 페이지를 요청시에 404 NOT_FOUND와 error 페이지를 반환한다.")
     @Test
-    @WithMockMember(role = MemberRole.ROLE_ADMIN)
-    @DisplayName("공지사항 수정 페이지 이동")
-    void updatePage() throws Exception {
-        // given
+    void requestDetailViewWithNonExistIdReturnNotFoundAndErrorView() throws Exception {
+        // Arrange
+        long noticeId = Long.MAX_VALUE;
+
+        given(noticeService.find(noticeId)).willThrow(new NoticeNotFound());
+
+        // Act & Assert
+        mockMvc.perform(get("/notices/{noticeId}", noticeId))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("error"))
+                .andExpect(model().attributeExists("error"));
+
+        then(noticeService).should(times(1))
+                .find(noticeId);
+    }
+
+    @DisplayName("공지사항 update 페이지 뷰를 반환한다.")
+    @Test
+    void returnUpdateView() throws Exception {
+        // Arrange
         long noticeId = 1L;
-        NoticeResponse mockNotice = NoticeResponse.builder()
-                .title("공지사항 제목")
-                .content("공지사항 내용")
-                .build();
+        NoticeResponse response = createNoticeResponse("title 1", "content 1",
+                LocalDateTime.of(2025, 1, 1, 0, 0, 0));
 
-        // when
-        when(noticeService.find(noticeId)).thenReturn(mockNotice);
+        given(noticeService.find(noticeId)).willReturn(response);
 
-        // expected
+        // Act & Assert
         mockMvc.perform(get("/notices/{noticeId}/update", noticeId))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name(VIEW_PATH + "/update"))
-                .andExpect(model().attributeExists("notice"))
-                .andExpect(model().attribute("notice", is(mockNotice)))
-                .andDo(print());
+                .andExpect(model().attributeExists("notice"));
+
+        then(noticeService).should(times(1))
+                .find(noticeId);
+    }
+
+    @DisplayName("존재하지 않는 ID로 update 페이지를 요청시에 404 NOT_FOUND와 error 페이지를 반환한다.")
+    @Test
+    void requestUpdateViewWithNonExistIdReturnNotFoundAndErrorView() throws Exception {
+        // Arrange
+        long noticeId = Long.MAX_VALUE;
+
+        given(noticeService.find(noticeId)).willThrow(new NoticeNotFound());
+
+        // Act & Assert
+        mockMvc.perform(get("/notices/{noticeId}/update", noticeId))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("error"))
+                .andExpect(model().attributeExists("error"));
+
+        then(noticeService).should(times(1))
+                .find(noticeId);
+    }
+
+    private NoticeResponse createNoticeResponse(String title, String content, LocalDateTime dateTime) {
+        return NoticeResponse.builder()
+                .title(title)
+                .content(content)
+                .member(mock(MemberResponse.class))
+                .createdAt(dateTime)
+                .build();
     }
 }
