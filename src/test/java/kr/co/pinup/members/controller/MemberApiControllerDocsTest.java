0620971be0e79c9a2adc4c129137d5e86655ad82
kimdohee58 <kimdohee58@gmail.com>
2025-06-15 18:58:23 +0900

    add : member.adoc

--- src/test/java/kr/co/pinup/members/controller/MemberApiControllerDocsTest.java
+++ src/test/java/kr/co/pinup/members/controller/MemberApiControllerDocsTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -39,13 +40,13 @@ import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = MemberApiController.class)
+@WebMvcTest(MemberApiController.class)
 @ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(outputDir = "build/generated-snippets")
 @Import({MemberApiControllerDocsTest.MockConfig.class, MemberApiControllerDocsTest.SecurityConfig.class, RestDocsSupport.class, LoggerConfig.class})
 class MemberApiControllerDocsTest {
 
