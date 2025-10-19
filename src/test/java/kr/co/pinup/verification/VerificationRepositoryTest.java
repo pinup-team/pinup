cdf73b18f314e03053a23e55f8a69c291518bdd7
kimdohee58 <kimdohee58@gmail.com>
2025-10-19 22:36:49 +0900

    fix : activeprofiles 추가

--- src/test/java/kr/co/pinup/verification/VerificationRepositoryTest.java
+++ src/test/java/kr/co/pinup/verification/VerificationRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -14,6 +15,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class VerificationRepositoryTest {
 
     @Autowired
